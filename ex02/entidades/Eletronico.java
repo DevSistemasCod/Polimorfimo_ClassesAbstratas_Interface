0b196287d361739e397113d974694ad244258b15
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-04-27 21:39:41 -0300

    Add files via upload

---
+++ ex02/entidades/Eletronico.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class Eletronico extends Item {
+	private String tensao;
+	
+    public Eletronico(int id, String nome, float  preco, float imposto, String tensao) {
+        super(id, nome, preco, imposto);
+        this.tensao = tensao;
+    }
+    
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Tensao: " + tensao);
+    }
+    
+    public float calcularPreco() {
+    	float resultado;
+    	resultado = (preco + (preco*imposto) + (preco*0.08f));
+    	
+        return resultado; 
+    }
+}
